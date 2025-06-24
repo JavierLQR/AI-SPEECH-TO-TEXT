8a7519c97cd4cef96398315769496e4ceb99951d
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-24 01:40:49 -0500

    refactor: remove unused DTOs and entity for Deepseek

--- src/ia-lanchaing/deepseek/dto/update-deepseek.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types'
-import { CreateDeepseekDto } from './create-deepseek.dto'
-
-export class UpdateDeepseekDto extends PartialType(CreateDeepseekDto) {}
