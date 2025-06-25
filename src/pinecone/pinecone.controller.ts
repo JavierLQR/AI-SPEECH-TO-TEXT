beaf4d7867354e728113b290bd844569dd241b58
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-25 00:36:31 -0500

    feat: create pinecone.controller.ts

---
+++ src/pinecone/pinecone.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get } from '@nestjs/common'
+import { PineconeService } from './pinecone.service'
+
+@Controller('pinecone')
+export class PineconeController {
+  constructor(private readonly pineconeService: PineconeService) {}
+
+  @Get('index')
+  getIndex() {
+    return this.pineconeService.createIndex()
+  }
+
+  @Get('upsert-index')
+  upsertIndex() {
+    return this.pineconeService.upsertIndex()
+  }
+
+  @Get('query-index')
+  queryIndex() {
+    return this.pineconeService.queryIndex('Black holes')
+  }
+}
