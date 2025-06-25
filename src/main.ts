c730d0b589cd1015c301afea4a4c9193e3f32147
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-24 22:41:57 -0500

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useLogger(
+    process.env.NODE_ENV === 'production'
+      ? ['error', 'warn']
+      : ['log', 'error', 'warn', 'debug', 'verbose'],
+  )
   await app.listen(process.env.PORT ?? 4000)
   console.log(`Application is running on: ${await app.getUrl()}`)
 }
