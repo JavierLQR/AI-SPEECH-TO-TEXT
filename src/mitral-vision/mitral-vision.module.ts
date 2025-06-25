a0fa9c6a593ccb9c6ff03c9d08ff9c60bc761017
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-25 16:07:51 -0500

    feat: create mitral-vision.module.ts

---
+++ src/mitral-vision/mitral-vision.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { MitralVisionService } from './mitral-vision.service';
+import { MitralVisionController } from './mitral-vision.controller';
+
+@Module({
+  controllers: [MitralVisionController],
+  providers: [MitralVisionService],
+})
+export class MitralVisionModule {}
