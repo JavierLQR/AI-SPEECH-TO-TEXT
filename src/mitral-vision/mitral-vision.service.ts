953d44718b0d19f355b3d42c4296b6d78fb5781b
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-26 01:19:13 -0500

    feat: implement image description functionality in MitralVisionService

--- src/mitral-vision/mitral-vision.service.ts
+++ src/mitral-vision/mitral-vision.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common'
+import { Mistral } from '@mistralai/mistralai'
+import { Inject, Injectable, Logger } from '@nestjs/common'
 
 @Injectable()
-export class MitralVisionService {}
+export class MitralVisionService {
+  private readonly logger: Logger = new Logger(MitralVisionService.name)
+  constructor(@Inject(Mistral.name) private readonly mistral: Mistral) {
+    this.logger.verbose('MistralVisionService initialized')
+  }
+
+  /**
+   *
+   * @returns
+   */
+  public async describeImage() {
+    const chatResponse = await this.mistral.chat.complete({
+      model: 'pixtral-12b',
+      messages: [
+        {
+          role: 'user',
+          content: [
+            { type: 'text', text: "What's in this image?" },
+            {
+              type: 'image_url',
+              imageUrl:
+                'https://tripfixers.com/wp-content/uploads/2019/11/eiffel-tower-with-snow.jpeg',
+            },
+          ],
+        },
+      ],
+    })
+    return {
+      responseText: chatResponse.choices[0].message.content,
+    }
+  }
+}
