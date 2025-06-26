09e844bf3aa4386fa7dc5e1a061c196290f5af95
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-26 01:19:07 -0500

    update mitral-vision.module.ts

--- src/mitral-vision/mitral-vision.module.ts
+++ src/mitral-vision/mitral-vision.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common'
 import { MitralVisionService } from './mitral-vision.service'
 import { MitralVisionController } from './mitral-vision.controller'
-
+import { Mistral } from '@mistralai/mistralai'
+import { ConfigService } from '@nestjs/config/dist/config.service'
 @Module({
   controllers: [MitralVisionController],
-  providers: [MitralVisionService],
+  providers: [
+    MitralVisionService,
+    {
+      provide: Mistral.name,
+      useFactory: (configService: ConfigService) =>
+        new Mistral({
+          apiKey: configService.getOrThrow<string>('MISTRAL_API_KEY'),
+        }),
+      inject: [ConfigService],
+    },
+  ],
 })
 export class MitralVisionModule {}
