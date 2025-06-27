671a68267e38f827fa530ea7f64a7b9acf4cdfc7
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-27 13:05:21 -0500

    update deepseek.controller.ts

--- src/ia-lanchaing/deepseek/deepseek.controller.ts
+++ src/ia-lanchaing/deepseek/deepseek.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller } from '@nestjs/common'
 import { DeepseekService } from './deepseek.service'
 
 @Controller('deepseek')
 export class DeepseekController {
   constructor(private readonly deepseekService: DeepseekService) {}
-
-  @Get()
-  findAll() {
-    return this.deepseekService.findAll()
-  }
 }
