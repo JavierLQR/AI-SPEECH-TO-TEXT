6dd915bda4d2289e5f6e9c7057fb41e7a4d6c740
rodrigo15975 <javierluis15975@hotmail.com>
2025-06-29 03:53:50 -0500

    feat: create lanchaing-memory-postgres.controller.ts

---
+++ src/lanchaing-memory-postgres/lanchaing-memory-postgres.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Query } from '@nestjs/common'
+import { LanchaingMemoryPostgresService } from './lanchaing-memory-postgres.service'
+
+@Controller('lanchaing-memory-postgres')
+export class LanchaingMemoryPostgresController {
+  constructor(
+    private readonly lanchaingMemoryPostgresService: LanchaingMemoryPostgresService,
+  ) {}
+
+  @Get('chat')
+  conversation(@Query('message') message: string) {
+    return this.lanchaingMemoryPostgresService.conversation(message)
+  }
+}
