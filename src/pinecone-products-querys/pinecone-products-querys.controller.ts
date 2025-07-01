56ccc5cae79e053eeecc3712931b03a81d56be5a
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-01 04:05:24 -0500

    update pinecone-products-querys.controller.ts

--- src/pinecone-products-querys/pinecone-products-querys.controller.ts
+++ src/pinecone-products-querys/pinecone-products-querys.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common'
 
 import { PineconeProductsQuerysService } from './pinecone-products-querys.service'
 
@@ -9,7 +9,26 @@ export class PineconeProductsQuerysController {
   ) {}
 
   @Get('create-index')
+  @HttpCode(HttpStatus.CREATED)
   createIndex() {
     return this.pineconeProductsQuerysService.createIndex()
+  }
+
+  @Get('products-index')
+  @HttpCode(HttpStatus.OK)
+  findAllProductsIndex() {
+    return this.pineconeProductsQuerysService.findAllProductsIndex()
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Get('insert-products-index')
+  insertEmbeddings() {
+    return this.pineconeProductsQuerysService.insertEmbeddings()
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get('query-products-index')
+  queryEmbeddings(@Query('text') text: string) {
+    return this.pineconeProductsQuerysService.queryEmbeddings(text)
   }
 }
