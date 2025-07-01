fc8fe75aa574150505e7864bfd620295c42cbba0
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-01 04:05:28 -0500

    update pinecone-products-querys.service.ts

--- src/pinecone-products-querys/pinecone-products-querys.service.ts
+++ src/pinecone-products-querys/pinecone-products-querys.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { PrismaService } from 'nestjs-prisma'
+import { ConfigService } from '@nestjs/config'
 
 import { CohereEmbeddings } from '@langchain/cohere'
 import { Pinecone as PineconeClient } from '@pinecone-database/pinecone'
-
-import { ConfigService } from '@nestjs/config'
+import { PineconeStore } from '@langchain/pinecone'
+import { Document } from '@langchain/core/documents'
 
 @Injectable()
 export class PineconeProductsQuerysService {
   private readonly embeddings: CohereEmbeddings
   private readonly pinecone: PineconeClient
+  private readonly nameIndex: string = 'products-querys'
+
+  private readonly logger: Logger = new Logger(
+    PineconeProductsQuerysService.name,
+  )
 
   constructor(
     private readonly prismaService: PrismaService,
@@ -17,7 +23,7 @@ export class PineconeProductsQuerysService {
   ) {
     this.embeddings = new CohereEmbeddings({
       apiKey: this.configService.getOrThrow<string>('COHERE_API_KEY'),
-      model: 'embed-v4.0',
+      model: this.configService.getOrThrow<string>('COHERE_MODEL_EMBED'),
     })
 
     this.pinecone = new PineconeClient({
@@ -27,7 +33,7 @@ export class PineconeProductsQuerysService {
 
   public async createIndex() {
     const index = await this.pinecone.createIndex({
-      name: 'products-querys',
+      name: this.nameIndex,
       vectorType: 'dense',
       dimension: 1536,
       metric: 'cosine',
@@ -41,7 +47,7 @@ export class PineconeProductsQuerysService {
       tags: {
         environment: 'development',
         model: 'embed-v4.0',
-        name: 'products-querys',
+        name: this.nameIndex,
         owner: 'rodrigo',
       },
     })
@@ -49,5 +55,71 @@ export class PineconeProductsQuerysService {
       message: 'Pinecone index created successfully',
       indexName: index,
     }
+  }
+
+  /**
+   * insertEmbeddings
+   */
+  public async insertEmbeddings() {
+    const productsIndex = await this.findAllProductsIndex()
+
+    const docs: Document[] = productsIndex.map(
+      ({ description, id, imageUrl, name, price }) => ({
+        pageContent: `${name} - ${description}`,
+        metadata: {
+          id,
+          description,
+          imageUrl,
+          name,
+          price,
+        },
+      }),
+    )
+
+    const vectorStore = await PineconeStore.fromDocuments(
+      docs,
+      this.embeddings,
+      {
+        namespace: 'dev',
+        pineconeIndex: this.pinecone.index(this.nameIndex),
+      },
+    )
+
+    return {
+      message: 'Productos vectorizados e insertados exitosamente',
+      vectorStore,
+    }
+  }
+
+  /**
+   * queryEmbeddings
+   */
+  public async queryEmbeddings(text: string) {
+    const vector = await this.embeddings.embedQuery(text)
+    const index = this.pinecone.index(this.nameIndex).namespace('dev')
+
+    const dataVector = await index.query({
+      vector,
+      topK: 5,
+      includeMetadata: true,
+    })
+
+    this.logger.debug(dataVector)
+
+    return {
+      message: 'Pinecone index queried successfully',
+      dataVector: dataVector.matches.map(({ metadata }) => metadata),
+    }
+  }
+
+  /**
+   * findAllProductsIndex
+   */
+  public async findAllProductsIndex() {
+    return await this.prismaService.productIndex.findMany({
+      skip: 0,
+      take: 100,
+      orderBy: { id: 'desc' },
+    })
   }
 }
