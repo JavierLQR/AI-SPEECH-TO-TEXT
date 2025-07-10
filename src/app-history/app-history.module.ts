cb9bec7081f6ca84c5852ca0cd3415dcdca2e4c6
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-10 03:46:22 -0500

    update app-history.module.ts

--- src/app-history/app-history.module.ts
+++ src/app-history/app-history.module.ts
@@ -4,9 +4,10 @@ import { AppHistoryController } from './app-history.controller'
 import { AppHistoryService } from './app-history.service'
 import { PineconeModule } from './pinecone/pinecone.module'
 import { CohereModelEmbedModule } from './cohere-model-embed/cohere-model-embed.module'
+import { MongoHistoryChatModule } from './mongo-history-chat/mongo-history-chat.module'
 
 @Module({
-  imports: [PineconeModule, CohereModelEmbedModule],
+  imports: [PineconeModule, CohereModelEmbedModule, MongoHistoryChatModule],
   controllers: [AppHistoryController],
   providers: [AppHistoryService],
 })
