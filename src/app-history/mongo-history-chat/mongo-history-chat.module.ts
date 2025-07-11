0654d41cc61e56a2ebb5b17eaa427342753caca8
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-11 03:46:21 -0500

    update mongo-history-chat.module.ts

--- src/app-history/mongo-history-chat/mongo-history-chat.module.ts
+++ src/app-history/mongo-history-chat/mongo-history-chat.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common'
 import { MongoHistoryChatService } from './mongo-history-chat.service'
-import { CohereModelEmbedModule } from '../cohere-model-embed/cohere-model-embed.module'
 
 @Module({
-  imports: [CohereModelEmbedModule],
+  imports: [],
   controllers: [],
   providers: [MongoHistoryChatService],
   exports: [MongoHistoryChatService],
