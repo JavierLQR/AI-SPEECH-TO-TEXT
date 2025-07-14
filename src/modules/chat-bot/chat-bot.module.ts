c2ac4e8bf219b2e2be9c0116cc5fe0496fa3b4b6
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-14 17:25:19 -0500

    update chat-bot.module.ts

--- src/modules/chat-bot/chat-bot.module.ts
+++ src/modules/chat-bot/chat-bot.module.ts
@@ -2,33 +2,26 @@ import { Module } from '@nestjs/common'
 import { ChatBotService } from './chat-bot.service'
 import { ChatBotController } from './chat-bot.controller'
 import { MongooseModule } from '@nestjs/mongoose'
-import { ConfigService } from '@nestjs/config'
+
 import {
   ChatHistoryUserEntity,
   ChatHistoryUserSchema,
 } from './entities/user-entity'
 import { MongoHistoryChatModule } from '../app-history/mongo-history-chat/mongo-history-chat.module'
+import { PineconeModule } from '../app-history/pinecone/pinecone.module'
+import { CohereModelEmbedModule } from '../app-history/cohere-model-embed/cohere-model-embed.module'
 
 @Module({
   imports: [
+    PineconeModule,
     MongoHistoryChatModule,
+    CohereModelEmbedModule,
     MongooseModule.forFeatureAsync([
       {
         name: ChatHistoryUserEntity.name,
         useFactory: () => ChatHistoryUserSchema,
       },
     ]),
-
-    MongooseModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.getOrThrow<string>('DATABASE_MONGO_URI'),
-        driverInfo: {
-          name: 'Langchaing',
-        },
-        ssl: true,
-      }),
-      inject: [ConfigService],
-    }),
   ],
   controllers: [ChatBotController],
   providers: [ChatBotService],
