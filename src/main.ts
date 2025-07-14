bf227ce2597af8f1f2fd65d538a6492eb62210d7
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-14 17:24:52 -0500

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+  })
   app.useLogger(
     process.env.NODE_ENV === 'production'
       ? ['error', 'warn']
