bd771683ea51d9c905a9befd14d472d1d9d78271
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-14 17:25:29 -0500

    update chat-bot.service.ts

--- src/modules/chat-bot/chat-bot.service.ts
+++ src/modules/chat-bot/chat-bot.service.ts
@@ -1,37 +1,97 @@
-import { MongoDBChatMessageHistory } from '@langchain/mongodb'
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 
-import { Collection } from 'mongodb'
-import { MongoHistoryChatService } from '../app-history/mongo-history-chat/mongo-history-chat.service'
+import { CohereModelEmbedService } from '../app-history/cohere-model-embed/cohere-model-embed.service'
+import { PineconeService } from '../app-history/pinecone/pinecone.service'
 import { ChatHistoryUserEntity } from './entities/user-entity'
 
 @Injectable()
 export class ChatBotService {
+  private readonly logger: Logger = new Logger(ChatBotService.name)
   constructor(
-    private readonly mongoHistoryChatService: MongoHistoryChatService,
     @InjectModel(ChatHistoryUserEntity.name)
     private readonly chatHistoryUserEntity: Model<ChatHistoryUserEntity>,
-  ) {}
 
-  private get getColletion(): Collection {
-    return this.mongoHistoryChatService.Colletion
-  }
+    private readonly pineconeService: PineconeService,
+    private readonly cohereModelEmbedService: CohereModelEmbedService,
+  ) {}
 
   /**
    * getMessages
    */
-  public async getMessages() {
-    const collection = new MongoDBChatMessageHistory({
-      collection: this.getColletion,
-      sessionId: 'chatbot',
+  public async getMessages(text: string) {
+    const chain = this.cohereModelEmbedService.withMemory('123456')
+
+    const modelEmbeddings = this.cohereModelEmbedService.embeddings
+
+    const pineconeIndex = this.pineconeService.getPineconeIndexName(
+      'list-products',
+      'dev',
+    )
+
+    const pineconeStore = this.pineconeService.getPineconeStore(
+      modelEmbeddings,
+      {
+        namespace: 'dev',
+        textKey: 'pageContent',
+        pineconeIndex,
+        maxConcurrency: 2,
+        maxRetries: 2,
+        onFailedAttempt: (error) => this.logger.error(error),
+      },
+    )
+
+    const similaritySearch = await pineconeStore.similaritySearchWithScore(
+      text,
+      5,
+    )
+
+    const context = similaritySearch.map(([doc]) => doc.pageContent).join('\n')
+
+    const response = await chain.stream(
+      {
+        context,
+        question: text,
+      },
+      {
+        configurable: {
+          sessionId: '123456',
+        },
+      },
+    )
+    console.log({
+      response,
     })
-    const messages = await collection.getMessages()
-    return {
-      messages,
-      sessionId: '123456',
+
+    let fullResponse: string = ''
+
+    for await (const text of response) {
+      const isString = typeof text === 'string'
+      if (!isString) continue
+      fullResponse += text
+      console.log({ text })
     }
+    console.log({
+      fullResponse,
+    })
+
+    // await this.chatHistoryUserEntity.create({
+    //   sessionId: '123456',
+    //   userQuestion: text,
+    //   assistantResponse: fullResponse,
+    //   retrievedProducts: similaritySearch.map(([{ metadata }, score]) => ({
+    //     name: metadata.name ?? '',
+    //     score,
+    //   })),
+    //   userId: '123456',
+    //   embeddingUsed: await modelEmbeddings.embedQuery(text),
+    //   messageId: '123456',
+    //   tokensUsed: 1000,
+    //   latencyMs: 1000,
+    //   promptTemplate: 'prompt',
+    //   modelName: 'Cohere',
+    // })
   }
 }
