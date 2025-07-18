ef86467d29bfdda5dfd8068f9b5e17253c68a3bc
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-17 23:19:26 -0500

    update chatbot-e-commerce.module.ts

--- src/apps/chatbot-e-commerce/chatbot-e-commerce.module.ts
+++ src/apps/chatbot-e-commerce/chatbot-e-commerce.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+
 import { ChatbotECommerceService } from './chatbot-e-commerce.service'
 import { ChatbotECommerceController } from './chatbot-e-commerce.controller'
 import { PineconeModule } from 'src/modules/app-history/pinecone/pinecone.module'
+import { MongoHistoryChatModule } from 'src/modules/app-history/mongo-history-chat/mongo-history-chat.module'
+import {
+  ChatHistoryUserEntity,
+  ChatHistoryUserSchema,
+} from 'src/modules/chat-bot/entities/user-entity'
 
 @Module({
-  imports: [PineconeModule],
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: ChatHistoryUserEntity.name,
+        useFactory: () => ChatHistoryUserSchema,
+      },
+    ]),
+    PineconeModule,
+    MongoHistoryChatModule,
+  ],
   controllers: [ChatbotECommerceController],
   providers: [ChatbotECommerceService],
 })
