3d377ee9f8a86c5149a3997a0519fac72d913ecd
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-18 23:33:42 -0500

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,6 +14,17 @@ async function bootstrap() {
   )
 
   app.useGlobalFilters(new HttpExceptionFilter())
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      errorHttpStatusCode: 422,
+    }),
+  )
 
   await app.listen(process.env.PORT ?? 4000)
   console.log(`Application is running on: ${await app.getUrl()}`)
