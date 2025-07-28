6c50407b6278e820d27c54c2c61d0c02ff4b6d92
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-27 21:36:43 -0500

    update chatbot-sessions.dto.ts

--- src/apps/chatbot-e-commerce/dto/chatbot-sessions.dto.ts
+++ src/apps/chatbot-e-commerce/dto/chatbot-sessions.dto.ts
@@ -1,4 +1,37 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator'
+import { Type } from 'class-transformer'
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator'
+
+class ChannelMessageDto {
+  @IsString()
+  @IsNotEmpty()
+  channelName: string
+
+  @IsString()
+  @IsNotEmpty()
+  eventName: string
+
+  @IsString()
+  @IsOptional()
+  id: string
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  message: string
+
+  @IsString()
+  @IsNotEmpty()
+  userId: string
+
+  @IsString()
+  @IsOptional()
+  timestamp: string
+}
 
 export class ChatbotSessionsDto {
   @IsString()
@@ -14,4 +47,10 @@ export class ChatbotSessionsDto {
   @IsNotEmpty()
   @IsOptional()
   question: string
+
+  @IsOptional()
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => ChannelMessageDto)
+  channelMessage: ChannelMessageDto
 }
