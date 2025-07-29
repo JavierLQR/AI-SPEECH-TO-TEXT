0fe9f322ce3930c88c4b31bdac413018e9c7e62f
rodrigo15975 <javierluis15975@hotmail.com>
2025-07-28 23:12:09 -0500

    update send-message-pusher.dto.ts

--- src/apps/pusher/dto/send-message-pusher.dto.ts
+++ src/apps/pusher/dto/send-message-pusher.dto.ts
@@ -1,12 +1,51 @@
-export class SendMessagePusherDto {
-  channelName: string
-  eventName: string
-  data: {
-    id: string
-    message: string
-    userId: string
-    timestamp: string
-  }
+import { Type } from 'class-transformer'
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator'
+
+class DataMessage {
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  id?: string
+
+  @IsNotEmpty()
+  @IsString()
+  message: string
+
+  @IsNotEmpty()
+  @IsString()
   userId: string
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  timestamp?: string | Date
+}
+
+export class SendMessagePusherDto {
+  @IsString()
+  @IsNotEmpty()
+  channelName: string
+
+  @IsNotEmpty()
+  @IsString()
+  eventName: string
+
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => DataMessage)
+  data: DataMessage
+
+  @IsNotEmpty()
+  @IsString()
+  userId: string
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
   socket_id?: string
 }
