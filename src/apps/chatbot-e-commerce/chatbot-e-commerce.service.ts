1992f0853cd77ec7e9ccf550e48d831011b2d643
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-05 23:22:23 -0500

    update chatbot-e-commerce.service.ts

--- src/apps/chatbot-e-commerce/chatbot-e-commerce.service.ts
+++ src/apps/chatbot-e-commerce/chatbot-e-commerce.service.ts
@@ -137,6 +137,10 @@ export class ChatbotECommerceService {
       const isString = typeof chunk === 'string'
       if (!isString) continue
       const { channelName, eventName, userId } = channelMessage
+      console.log({
+        chunk,
+      })
+
       await this.pusherService.trigger({
         channelName,
         eventName,
